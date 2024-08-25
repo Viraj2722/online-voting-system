1b1c0964c8af955995bc02b914e63dcc5b9da800
Bhavik  Solanki <141156721+bhavik8455@users.noreply.github.com>
2024-08-25 18:28:57 +0530

    changes updated

--- myapp/settings.py
+++ myapp/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'GEGOTBQXQNYBEfQuIjoHLYaWLkzZayHe',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '22332',
     }
 }
+
 
 
 # Password validation
--- votingsystem/views.py
+++ votingsystem/views.py
@@ -1,4 +1,20 @@
+
 from django.shortcuts import render, redirect
+import os ,psycopg2
+import easygui as e
+from dotenv import load_dotenv
+load_dotenv()
+
+
+connection = psycopg2.connect(database=os.getenv("DATABASE_NAME"), 
+                              user=os.getenv("DATABASE_USER"),
+                                password=os.getenv("DATABASE_PASSWORD"),
+                                  host=os.getenv("DATABASE_HOST"),
+                                    port=os.getenv("DATABASE_PORT"))
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM voter")
+record = cursor.fetchall()
+# print(record)
 
 def home(request):
     return render(request, 'home.html')
@@ -6,22 +22,50 @@ def home(request):
 def candidate_login(request):
     if request.method == 'POST':
         voter_id = request.POST['voter_id']
-        dob = request.POST['dob']
-        # Add logic to authenticate voter
-        return redirect('home')
+        mobileno = request.POST['mobileno']
+
+        cursor.execute("SELECT * FROM voter WHERE voterid=%s AND voter_number=%s", (voter_id, mobileno))
+        record = cursor.fetchone()
+        
+        # Check if the voter exists
+        if record:
+            return redirect('candidatelist')
+        else:
+            e.msgbox('Invalid Voter ID or Mobile Number', 'Error')
+            return render(request, 'candidatelogin.html')
+            
+
     return render(request, 'candidatelogin.html')
 
+    
+    
 def admin_login(request):
+    if request.method == 'POST':
+        adminid =  request.POST['admin_id']
+        
+        cursor.execute("SELECT * FROM voteradmin WHERE adminid = '{adminid}' ".format(adminid=adminid))
+        record = cursor.fetchone()
+
+        
+        # Check if the admin exists
+        if record:
+            return redirect('adminpage')
+        else:
+            e.msgbox('Invalid Admin ID ', 'Error')
+            return render(request, 'adminlogin.html')
+    
+    return render(request, 'adminlogin.html')
+
+def admin_page(request):
     if request.method == 'POST':
         leader_name = request.POST['leader_name']
         symbol = request.FILES['symbol']
         # Add logic to save political leader and symbol
         return redirect('home')
-    return render(request, 'adminlogin.html')
 
-def admin_page(request):
     return render(request, 'adminpage.html')
 
 def candidate_list(request):
     return render(request, 'candidatelist.html')
+
 
