90bbbf85b005ddc0b24512bad5cd4ccc8b2788d5
Bhavik  Solanki <141156721+bhavik8455@users.noreply.github.com>
2024-09-07 14:14:54 +0530

    Added the logic for casting the vote

--- votingsystem/urls.py
+++ votingsystem/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('candidatelogin/', views.candidate_login, name='candidatelogin'),
     path('candidatelist/', views.candidate_list, name='candidatelist'),
     path('adminpage/', views.admin_page, name='adminpage'),
+    path('cast_vote/', views.cast_vote, name='cast_vote'),
     path('adminlogin/', views.admin_login, name='adminlogin')
 ]
